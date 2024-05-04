638d6549936f588673dfa9fbb8a9b52926ad60b2
RalphRG <Orangespirit00@gmail.com>
2024-05-04 15:06:50 -0600

    added img and name

---
+++ pokemonAPI/ReadMe.md
@@ -0,0 +1,8 @@
+# Pokemon API
+ Just a Simple search for pokemons using pokemon https://pokeapi.co/api
+ 
+ I can probably include some stats when they are searched not just display the name and picture but this will do for now.
+
+## Things I want to improve but don't know how
+
+ 1. I wanted to make it so that the background of the webpage is the same color as the dominant color of the pokemon
--- pokemonAPI/script.js
+++ pokemonAPI/script.js
@@ -1,44 +1,36 @@
 console.log("WORKS");
 
-/* `<section class="card">
-<h2 class="name"></h2>
-<img src="" alt="pokemon" class="pokemon-img">
-</section>`; */
 
-// fetch('https://pokeapi.co/api/v2/pokemon/ditto').then(response => response.json()).then(data => console.log(data)).catch(error => console.error(error));
-/* 
-fetch(`https://pokeapi.co/api/v2/pokemon/${name}`).then(response => {
-        
-if(!response.ok){
-    throw new Error(`${name} does not exist`)
-}
-return response.json();
-}).then(data => {console.log(data.name)
+// show the image and the name
+function card  (name, img){
+    let cardContainer = document.querySelector('.card-container');
+    name = name.substring(0,1).toUpperCase() + name.substring(1);
+    cardContainer.innerHTML =`<section class="card">
+    <h2 class="pokemon-name">${name}</h2>
+    <img src="${img}" alt="pokemon" class="pokemon-sprite">
+    </section>`;
+};
+
+//Fetching Data
+async function getPokemon(name) {
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+
+    if (!response.ok) {
+      throw new Error(`could not find ${name}`);
+    }
     
-
-}).catch(error => console.error(error)); */
-
-
-async function getPokemon(name){
-    try{
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
-
-        if(!response.ok){
-            throw new Error(`could not find ${name}`);
-        }
-        const data = response.json();
-        const pokemonImage = data.sprites.front_default;
-        
-
-    }
-    catch(error){
-        console.error(error);
-    }
+    const data = await response.json();
+    const pokemonImage = data.sprites.front_default;
+    card(data.name,pokemonImage);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-const searchButton = document.querySelector('.search-pok');
-
-searchButton.addEventListener('click', () => {
-    const pokeName = document.getElementById('search').value.toLowerCase();
-    getPokemon(pokeName);
-})
+const searchButton = document.querySelector(".search-pok");
+//when the button is clicked fetch the data
+searchButton.addEventListener("click", () => {
+  const pokeName = document.getElementById("search").value.toLowerCase();
+  getPokemon(pokeName);
+});
