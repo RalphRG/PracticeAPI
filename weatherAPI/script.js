effad6f6627d5be1a5c642560e0df82945006605
RalphRG <Orangespirit00@gmail.com>
2024-05-07 16:14:46 -0600

    designed card container

--- weatherAPI/script.js
+++ weatherAPI/script.js
@@ -1,3 +1,6 @@
 console.log('works')
 
-fetch('api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key} ')
+// fetch('api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key} ')
+
+const card = document.querySelector('.card-container.hidden');
+card.classList.remove('hidden');
