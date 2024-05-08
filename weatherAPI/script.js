f2b18ad30737cbf19884e8477ec632676bd69b28
RalphRG <Orangespirit00@gmail.com>
2024-05-07 18:45:45 -0600

    added fetch

--- weatherAPI/script.js
+++ weatherAPI/script.js
@@ -1,6 +1,34 @@
-console.log('works')
+console.log("works");
+const apiKey = 'c7ec8627afdc5f39d2b3cd73d5977caa';
 
-// fetch('api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key} ')
+//fetch(`http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=${apiKey}`)
 
-const card = document.querySelector('.card-container.hidden');
-card.classList.remove('hidden');
+
+const search = document.querySelector(".btn-svg");
+search.addEventListener("click", () => {
+
+  const card = document.querySelector(".card-container.hidden");card.classList.remove('hidden');
+    getWeather();
+});
+
+//fetch weather data
+
+async function getWeather(){
+    try{
+         let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=edmonton,ca&appid=c7ec8627afdc5f39d2b3cd73d5977caa&units=metric`)
+         const data = await response.json();
+
+        if(!!response.cod){
+            console.log('no fetch')
+        }
+
+         console.log(data);
+         console.log(data.cod)
+         console.log('this is the name',data.name)
+    }catch(error){
+        console.error(error)
+    }
+
+
+
+}
